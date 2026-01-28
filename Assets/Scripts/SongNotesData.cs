eb8038885f4f0bdabbdf2f824677de626c100360
MartinPlascencia <martin.plascencia@live.com.mx>
2026-01-27 20:47:57 -0600

    Sistema Notas integrado

--- Assets/Scripts/SongNotesData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SongNotesData", menuName = "Scriptable Objects/SongNotesData")]
-public class SongNotesData : ScriptableObject
-{
-    public NoteData[] notes;
-}
